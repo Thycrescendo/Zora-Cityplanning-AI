c488868959e2bdbfc4ff08446d54a9475ec69b04
thycrescendo <dmicholn@gmail.com>
2024-11-25 20:30:52 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, redisUrl: REDIS_URL });
-const transactionMiner = new TransactionMiner({
-  
+const transactionMiner = new Transact
 });
 
 setTimeout(() => pubsub.broadcastChain(), 1500);
